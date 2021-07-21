46a8505c6763a222cb6e74359087be06bfcebe37
Raydel C. Isasi <raydel.isasi92@gmail.com>
2021-07-21 00:35:44 -0400

    updated exception handler

--- src/main/java/raydel/isasi/movieinfoservice/exception/CustomExceptionHandler.java
+++ src/main/java/raydel/isasi/movieinfoservice/exception/CustomExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import raydel.isasi.movieinfoservice.pojo.ErrorResponse;
@@ -20,16 +21,13 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(value
-            = {Exception.class})
+            = {HttpClientErrorException.class})
     @ResponseBody
-    protected ResponseEntity<Object> handleException(
+    protected ResponseEntity<Object> handleNotFoundException(
             RuntimeException ex, WebRequest request) {
 
-        return new ResponseEntity<Object>(new ErrorResponse(ex.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Object>(new ErrorResponse(ex.getLocalizedMessage()),((HttpClientErrorException)ex).getStatusCode() );
     }
-
-
-
 
 
     @ExceptionHandler(value
